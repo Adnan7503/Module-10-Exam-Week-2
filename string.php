21fef27fcb2a797fa7f4edcbf296ce3554eeffdd
Adnan Sanib <96364300+Adnan7503@users.noreply.github.com>
2024-08-21 09:56:04 +0600

    Add files via upload

---
+++ string.php
@@ -0,0 +1,27 @@
+<?php
+function countVowels($str) {
+    $str = strtolower($str);
+    $vowels = array('a', 'e', 'i', 'o', 'u'); 
+    $vowelCount = 0;
+
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (in_array($str[$i], $vowels)) {
+            $vowelCount++;
+        }
+    }
+
+    return $vowelCount;
+}
+
+function reverseString($str) {
+    return strrev($str);
+}
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+foreach ($strings as $string) {
+    $vowelCount = countVowels($string);
+    $reversedString = reverseString($string);
+    echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reversedString\n";
+}
+?>
